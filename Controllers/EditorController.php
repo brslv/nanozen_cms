fa5dafb49070609c6df5a41825c0f3bf8b421326
brslv <brslvg@gmail.com>
2015-09-28 19:54:46 +0300

    add kint for convinient debugging.

---
+++ Controllers/EditorController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Nanozen\Controllers;
+
+use Nanozen\Providers\Controller\BaseControllerProvider as BaseController;
+
+/**
+ * Class EditorController
+ *
+ * @author brslv
+ * @package Nanozen\Controllers 
+ */
+class EditorController extends BaseController
+{
+
+	public function index()
+	{
+		
+	}
+	
+}
--- Views/includes/footer.php
+++ Views/includes/footer.php
@@ -2,7 +2,7 @@
 		<hr />
 
 		<div class="container" style="margin-bottom: 20px">
-			&copy; Copyright <?= date('Y'); ?> &mdash; <a href="">Sitename</a> is
+			&copy; Copyright <?= date('Y'); ?> &mdash; <a href="/"><?= $appTitle; ?></a> is
 			made with <a href="http://github.com/brslv/nanozen">NanozenCMS</a>
 		</div>
 	</footer>
